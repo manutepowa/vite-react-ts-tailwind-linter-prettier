c87fa07cf3e2b63a27661dd641b862a8de699cd3
Manuel <manutepowa@gmail.com>
2022-08-04 12:56:54 +0200

    Update vite version

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@ import react from "@vitejs/plugin-react"
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 3000,
+  },
   // define: {
   //   "process.env": {},
   //   global: {},
