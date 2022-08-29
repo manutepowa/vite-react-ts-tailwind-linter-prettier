cc68017bdb5b89d394abfe9b8b1697de12dc737e
Manuel <manutepowa@gmail.com>
2022-08-29 18:53:43 +0200

    Add new page not found

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import { Route, Switch } from "wouter"
 import Home from "./pages/Home"
 import "./App.css"
+import React from "react"
+import { NotFound } from "./pages/NotFound"
 
 function App(): React.ReactNode {
   return (
     <Switch>
       <Route path="/" component={Home} />
+      <Route>
+        <NotFound />
+      </Route>
     </Switch>
   )
 }
---
+++ src/pages/NotFound.tsx
@@ -0,0 +1,7 @@
+export const NotFound = () => {
+  return (
+    <div className="h-screen flex justify-center items-center text-black text-4xl">
+      <h1>Page not found</h1>
+    </div>
+  )
+}
