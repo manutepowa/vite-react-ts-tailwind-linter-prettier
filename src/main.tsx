14f80371ec2ecd6bf78786fca856b9edd2b09611
Manuel <manutepowa@gmail.com>
2023-04-08 13:42:37 +0200

    update: packages

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client"
 import App from "./App"
 import "./index.css"
 
+// eslint-disable-next-line prettier/prettier
 const root = document.getElementById("root") as HTMLElement
 
 ReactDOM.createRoot(root).render(
