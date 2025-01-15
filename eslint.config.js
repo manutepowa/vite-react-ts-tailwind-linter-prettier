35fb50a83f9ee873aa41eb8f8c2d9e593b75d499
Manuel <manutepowa@gmail.com>
2025-01-15 19:09:49 +0100

    update dependencies

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,7 @@
-import eslint from "@eslint/js"
 import tseslint from "typescript-eslint"
 import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended"
 
 export default tseslint.config(
-  eslint.configs.recommended,
   ...tseslint.configs.recommended,
   eslintPluginPrettierRecommended,
   {
@@ -26,11 +24,7 @@ export default tseslint.config(
           endOfLine: "auto",
           useTabs: false,
         },
-        {
-          usePrettierrc: false,
-        },
       ],
     },
   },
 )
-
